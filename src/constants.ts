ac0f9303af642896a36a21a04894a022d582d686
tunafish2k <lyt080529@163.com>
2026-02-13 23:04:23 +0800

    feat: 存储请求用常量

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const SECRET = "18comicAPP";
+export const SECRET_CONTENT = "18comicAPPContent";
+export const SECRET_APP_DATA = "185Hcomic3PAPP7R";
+export const SECRET_DOMAIN_SERVER = "diosfjckwpqpdfjkvnqQjsik";
+
+export const DOMAIN_SERVER_URL = [
+    "https://rup4a04-c01.tos-ap-southeast-1.bytepluses.com/newsvr-2025.txt",
+    "https://rup4a04-c02.tos-cn-hongkong.bytepluses.com/newsvr-2025.txt",
+];
+
+export const HEADERS_API = {
+    "Accept-Encoding": "gzip, deflate",
+    "user-agent":
+        "Mozilla/5.0 (Linux; Android 9; V1938CT Build/PQ3A.190705.11211812; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Safari/537.36",
+};
