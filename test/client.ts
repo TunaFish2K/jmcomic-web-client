096f722391a02a9dc04e26088486e09637357d73
tunafish2k <lyt080529@163.com>
2026-02-14 01:12:46 +0800

    chore: 为客户端添加简易测试

---
+++ test/client.ts
@@ -0,0 +1,18 @@
+import {
+    getClientDataAndCreateClient,
+    getFastestAvailableBaseURL,
+} from "../src/client";
+
+async function start() {
+    const baseURL = await getFastestAvailableBaseURL();
+    console.log("baseURL:", baseURL);
+    const client = await getClientDataAndCreateClient(baseURL!);
+    const searchResult = await client.search("蔚蓝档案");
+    console.log("搜索结果：", searchResult);
+    const album = await client.getAlbum("1235125");
+    console.log("获取的本子：", album);
+    const photo = await client.getAlbum("1235125");
+    console.log("获取的章节：", photo);
+}
+
+start();
