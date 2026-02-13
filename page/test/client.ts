3e26bad9ab458ab64cbdf8b25ccd84aeebc70e3b
tunafish2k <lyt080529@163.com>
2026-02-14 02:39:48 +0800

    fix: 修复client测试导入不正确

--- page/test/client.ts
+++ page/test/client.ts
@@ -1,7 +1,7 @@
 import {
     getClientDataAndCreateClient,
     getFastestAvailableBaseURL,
-} from "../src/client";
+} from "../src/modules/client";
 
 async function start() {
     const baseURL = await getFastestAvailableBaseURL();
