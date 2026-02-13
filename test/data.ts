5c91e3f32c767655f3fcfccb00cc811fd589a073
tunafish2k <lyt080529@163.com>
2026-02-14 01:34:20 +0800

    feat: 实现获取切片数量

--- test/data.ts
+++ test/data.ts
@@ -1,4 +1,6 @@
 import pLimit from "p-limit";
+import md5 from "crypto-js/md5";
+import encodingHex from "crypto-js/enc-hex";
 
 async function downloadImage({ name, url }: { name: string; url: string }) {
     try {
@@ -29,4 +31,20 @@ export async function downloadAllImages(
     const tasks = images.map((v) => limit(() => downloadImage(v)));
     const result = await Promise.all(tasks);
     return result;
+}
+export function getSliceCount(
+    scrambleId: number,
+    photoId: number,
+    filename: string,
+): number {
+    if (photoId < scrambleId) return 0;
+    if (filename.endsWith(".gif")) return 0;
+    if (photoId < 268850) return 10;
+
+    const hex = md5(`${photoId}${filename.split(".")[0]}`).toString(
+        encodingHex,
+    );
+    return (
+        (hex.charCodeAt(hex.length - 1) % (photoId < 421926 ? 10 : 8)) * 2 + 2
+    );
 }
