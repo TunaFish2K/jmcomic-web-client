7e792fcbfd723c227a1a23811ccee232274f9fac
tunafish2k <lyt080529@163.com>
2026-02-13 23:19:38 +0800

    feat: 完成解密算法

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,9 @@
 import md5 from "crypto-js/md5";
 import encodingHex from "crypto-js/enc-hex";
+import aes from "crypto-js/aes";
+import modeECB from "crypto-js/mode-ecb";
+import noPadding from "crypto-js/pad-nopadding";
+import encodingUTF8 from "crypto-js/enc-utf8";
 
 export const client = new (class Client {
     getToken(timestampSeconds: number, secret: string) {
@@ -7,5 +11,22 @@ export const client = new (class Client {
     }
     getTokenParam(timestampSeconds: number, version: string) {
         return `${timestampSeconds},${version}`;
+    }
+    decryptResponseData(data: string, secret: string) {
+        const decrypted = aes.decrypt(data, CryptoJS.enc.Utf8.parse(secret), {
+            mode: modeECB,
+            padding: noPadding,
+        });
+
+        let decryptedString = decrypted.toString(encodingUTF8);
+
+        const padLength = decryptedString.charCodeAt(
+            decryptedString.length - 1,
+        );
+        if (padLength && padLength <= 16) {
+            decryptedString = decryptedString.slice(0, -padLength);
+        }
+
+        return decryptedString;
     }
 })();
