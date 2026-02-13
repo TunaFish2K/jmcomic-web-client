193a5405141595b3e82eec7aaff543b0b50bbbd9
tunafish2k <lyt080529@163.com>
2026-02-14 00:10:10 +0800

    refactor: 将部分函数与Client解耦

--- src/client.ts
+++ src/client.ts
@@ -13,110 +13,116 @@ import {
 import pLimit from "p-limit";
 import parse, { parse as parseSetCookie } from "set-cookie-parser";
 
-export const client = new (class Client {
-    getToken(timestampSeconds: number, secret: string) {
-        return md5(`${timestampSeconds}${secret}`).toString(encodingHex);
-    }
-    getTokenParam(timestampSeconds: number, version: string) {
-        return `${timestampSeconds},${version}`;
-    }
-    decryptResponseData(encryptedData: string, secret: string) {
-        const decrypted = aes.decrypt(
-            encryptedData,
-            CryptoJS.enc.Utf8.parse(secret),
-            {
-                mode: modeECB,
-                padding: noPadding,
-            },
-        );
+function getToken(timestampSeconds: number, secret: string) {
+    return md5(`${timestampSeconds}${secret}`).toString(encodingHex);
+}
+function getTokenParam(timestampSeconds: number, version: string) {
+    return `${timestampSeconds},${version}`;
+}
+function decryptResponseData(encryptedData: string, secret: string) {
+    const decrypted = aes.decrypt(
+        encryptedData,
+        CryptoJS.enc.Utf8.parse(secret),
+        {
+            mode: modeECB,
+            padding: noPadding,
+        },
+    );
 
-        let decryptedString = decrypted.toString(encodingUTF8);
+    let decryptedString = decrypted.toString(encodingUTF8);
 
-        const padLength = decryptedString.charCodeAt(
-            decryptedString.length - 1,
-        );
-        if (padLength && padLength <= 16) {
-            decryptedString = decryptedString.slice(0, -padLength);
-        }
+    const padLength = decryptedString.charCodeAt(decryptedString.length - 1);
+    if (padLength && padLength <= 16) {
+        decryptedString = decryptedString.slice(0, -padLength);
+    }
 
-        return decryptedString;
+    return decryptedString;
+}
+async function getDomainsFromDomainServer(domainServerURL: string) {
+    const res = await fetch(domainServerURL);
+    const encryptedData = await res.text();
+    const secret = md5(SECRET_DOMAIN_SERVER).toString(encodingHex);
+    const decryptedData = decryptResponseData(encryptedData, secret);
+    const obj = JSON.parse(decryptedData);
+    return obj.Server as string[];
+}
+async function checkDomainStatus(domain: string, timeoutMs: number = 5000) {
+    try {
+        const url = `https://${domain}`;
+        const controller = new AbortController();
+        const timeout = setTimeout(() => {
+            controller.abort();
+        }, timeoutMs);
+        const startMs = performance.now();
+        await fetch(url, {
+            mode: "no-cors",
+            signal: controller.signal,
+        });
+        const endMs = performance.now();
+        clearTimeout(timeout);
+        return {
+            domain,
+            available: true as const,
+            timeMs: endMs - startMs,
+        };
+    } catch {
+        return {
+            domain,
+            available: false as const,
+            timeMs: null,
+        };
     }
-    async getDomainsFromDomainServer(domainServerURL: string) {
-        const res = await fetch(domainServerURL);
-        const encryptedData = await res.text();
-        const secret = md5(SECRET_DOMAIN_SERVER).toString(encodingHex);
-        const decryptedData = this.decryptResponseData(encryptedData, secret);
-        const obj = JSON.parse(decryptedData);
-        return obj.Server as string[];
+}
+async function pickFastestAvailableDomainToBaseURL(
+    domains: string[],
+    checkConcurrency: number = 20,
+) {
+    const limit = pLimit(checkConcurrency);
+    const tasks = domains.map((v) => limit(() => checkDomainStatus(v)));
+    const checkResult = await Promise.all(tasks);
+    const unavailableFiltered = checkResult.filter((v) => v.available);
+    const fastestSorted = unavailableFiltered.sort(
+        (a, b) => a.timeMs - b.timeMs,
+    );
+    const theFastestAvailable = fastestSorted[0] ?? null;
+    return theFastestAvailable !== null
+        ? `https://${theFastestAvailable}`
+        : null;
+}
+function getCurrentTimestampSeconds() {
+    return Math.floor(Date.now() / 1000);
+}
+function getCookieHeader(setCookie: parse.CookieMap) {
+    const result: string[] = [];
+
+    for (const [name, { value }] of Object.entries(setCookie)) {
+        result.push(`${name}=${value}`);
     }
-    async checkDomainStatus(domain: string, timeoutMs: number = 5000) {
-        try {
-            const url = `https://${domain}`;
-            const controller = new AbortController();
-            const timeout = setTimeout(() => {
-                controller.abort();
-            }, timeoutMs);
-            const startMs = performance.now();
-            await fetch(url, {
-                mode: "no-cors",
-                signal: controller.signal,
-            });
-            const endMs = performance.now();
-            clearTimeout(timeout);
-            return {
-                domain,
-                available: true as const,
-                timeMs: endMs - startMs,
-            };
-        } catch {
-            return {
-                domain,
-                available: false as const,
-                timeMs: null,
-            };
-        }
+
+    return result.join("; ");
+}
+export class Client {
+    baseURL: string;
+    constructor(baseURL: string) {
+        this.baseURL = baseURL;
     }
-    async pickFastestAvailableDomainToBaseURL(
-        domains: string[],
-        checkConcurrency: number = 20,
-    ) {
-        const limit = pLimit(checkConcurrency);
-        const tasks = domains.map((v) =>
-            limit(() => this.checkDomainStatus(v)),
-        );
-        const checkResult = await Promise.all(tasks);
-        const unavailableFiltered = checkResult.filter((v) => v.available);
-        const fastestSorted = unavailableFiltered.sort(
-            (a, b) => a.timeMs - b.timeMs,
-        );
-        const theFastestAvailable = fastestSorted[0] ?? null;
-        return theFastestAvailable !== null
-            ? `https://${theFastestAvailable}`
-            : null;
-    }
-    getCurrentTimestampSeconds() {
-        return Math.floor(Date.now() / 1000);
-    }
-    async getClientData(baseURL: string) {
-        const url = new URL("/setting", baseURL);
-        const timestampSeconds = this.getCurrentTimestampSeconds();
+    async getClientData() {
+        const url = new URL("/setting", this.baseURL);
+        const timestampSeconds = getCurrentTimestampSeconds();
         const res = await fetch(url, {
             headers: {
-                token: this.getToken(timestampSeconds, SECRET),
-                tokenparam: this.getTokenParam(
-                    timestampSeconds,
-                    INITIAL_VERSION,
-                ),
+                token: getToken(timestampSeconds, SECRET),
+                tokenparam: getTokenParam(timestampSeconds, INITIAL_VERSION),
             },
         });
         const setCookie = res.headers.getSetCookie();
         const setCookieData = parseSetCookie(setCookie, { map: true });
-        const cookie = this.getCookieHeader(setCookieData);
+        const cookie = getCookieHeader(setCookieData);
 
         const encryptedData = (await res.json()).data as string;
-        const decryptedData = this.decryptResponseData(
+        const decryptedData = decryptResponseData(
             encryptedData,
-            this.getToken(timestampSeconds, SECRET_APP_DATA),
+            getToken(timestampSeconds, SECRET_APP_DATA),
         );
         const { version, img_host: imageBaseURL } = JSON.parse(
             decryptedData,
@@ -127,13 +133,4 @@ export const client = new (class Client {
             cookie,
         };
     }
-    getCookieHeader(setCookie: parse.CookieMap) {
-        const result: string[] = [];
-
-        for (const [name, { value }] of Object.entries(setCookie)) {
-            result.push(`${name}=${value}`);
-        }
-
-        return result.join("; ");
-    }
-})();
+}
