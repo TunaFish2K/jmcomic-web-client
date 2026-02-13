c653df446c19a71bab4a933b1c15ae26cf3962ab
tunafish2k <lyt080529@163.com>
2026-02-13 23:43:26 +0800

    feat: 实现了可用域名测试

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,8 @@ import aes from "crypto-js/aes";
 import modeECB from "crypto-js/mode-ecb";
 import noPadding from "crypto-js/pad-nopadding";
 import encodingUTF8 from "crypto-js/enc-utf8";
+import { SECRET_DOMAIN_SERVER } from "./constants";
+import pLimit from "p-limit";
 
 export const client = new (class Client {
     getToken(timestampSeconds: number, secret: string) {
@@ -12,11 +14,15 @@ export const client = new (class Client {
     getTokenParam(timestampSeconds: number, version: string) {
         return `${timestampSeconds},${version}`;
     }
-    decryptResponseData(data: string, secret: string) {
-        const decrypted = aes.decrypt(data, CryptoJS.enc.Utf8.parse(secret), {
-            mode: modeECB,
-            padding: noPadding,
-        });
+    decryptResponseData(encryptedData: string, secret: string) {
+        const decrypted = aes.decrypt(
+            encryptedData,
+            CryptoJS.enc.Utf8.parse(secret),
+            {
+                mode: modeECB,
+                padding: noPadding,
+            },
+        );
 
         let decryptedString = decrypted.toString(encodingUTF8);
 
@@ -28,5 +34,56 @@ export const client = new (class Client {
         }
 
         return decryptedString;
+    }
+    async getDomainsFromDomainServer(domainServerURL: string) {
+        const res = await fetch(domainServerURL);
+        const encryptedData = await res.text();
+        const secret = md5(SECRET_DOMAIN_SERVER).toString(encodingHex);
+        const decryptedData = this.decryptResponseData(encryptedData, secret);
+        const obj = JSON.parse(decryptedData);
+        return obj.Server as string[];
+    }
+    async checkDomainStatus(domain: string, timeoutMs: number = 5000) {
+        try {
+            const url = `https://${domain}`;
+            const controller = new AbortController();
+            const timeout = setTimeout(() => {
+                controller.abort();
+            }, timeoutMs);
+            const startMs = performance.now();
+            await fetch(url, {
+                mode: "no-cors",
+                signal: controller.signal,
+            });
+            const endMs = performance.now();
+            clearTimeout(timeout);
+            return {
+                domain,
+                available: true as const,
+                timeMs: endMs - startMs,
+            };
+        } catch {
+            return {
+                domain,
+                available: false as const,
+                timeMs: null,
+            };
+        }
+    }
+    async pickFastestAvailableDomain(
+        domains: string[],
+        checkConcurrency: number = 20,
+    ) {
+        const limit = pLimit(checkConcurrency);
+        const tasks = domains.map((v) =>
+            limit(() => this.checkDomainStatus(v)),
+        );
+        const checkResult = await Promise.all(tasks);
+        const unavailableFiltered = checkResult.filter((v) => v.available);
+        const fastestSorted = unavailableFiltered.sort(
+            (a, b) => a.timeMs - b.timeMs,
+        );
+        const theFastestAvailable = fastestSorted[0] ?? null;
+        return theFastestAvailable;
     }
 })();
