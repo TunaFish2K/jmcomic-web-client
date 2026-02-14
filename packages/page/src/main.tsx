0d88c6a572aa084708eca11363e4c476beefda97
tunafish2k <lyt080529@163.com>
2026-02-14 15:24:30 +0800

    feat: 引入路由并加入主页

--- packages/page/src/main.tsx
+++ packages/page/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./home";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <Home></Home>
+        </BrowserRouter>
     </StrictMode>,
 );
