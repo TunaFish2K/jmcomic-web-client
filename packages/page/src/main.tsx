6b6c55e938249e52e3174a91deb40d60f56714bb
tunafish2k <lyt080529@163.com>
2026-02-14 18:38:16 +0800

    feat: 引入queryClient

--- packages/page/src/main.tsx
+++ packages/page/src/main.tsx
@@ -3,14 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                {" "}
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    {" "}
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     </StrictMode>,
 );
